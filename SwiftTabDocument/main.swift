c3546c3608debf2ac909865bddab40c1e7499c4a
Scott Horn <scott.horn@wotifgroup.com>
2014-06-04 18:10:26 +1000

    Initial Commit

---
+++ SwiftTabDocument/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  SwiftTabDocument
+//
+//  Created by Scott Horn on 4/06/2014.
+//  Copyright (c) 2014 Scott Horn. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
